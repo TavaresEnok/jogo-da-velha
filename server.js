8c7b5951f9d51a098b04350acbb6f64890456425
Renisson Silva <renissonx@gmail.com>
2020-10-25 14:44:33 -0300

    Mudando as divs para variáveis

--- server.js
+++ server.js
@@ -5,7 +5,22 @@ const app = express();
 
 app.get('/', function(req, res) {
   fs.readFile('index.html', 'utf-8', function(err, data) {
-    console.log('teste');
+    var quadrado = `
+        <a href="/">
+            <div class="quadrado"></div>
+        </a>`;
+    for (i = 1; i <= 9; i++) {
+        quadrado;
+    }
+    data = data.replace(/{{QUADRADO1}}/, quadrado);
+    data = data.replace(/{{QUADRADO2}}/, quadrado);
+    data = data.replace(/{{QUADRADO3}}/, quadrado);
+    data = data.replace(/{{QUADRADO4}}/, quadrado);
+    data = data.replace(/{{QUADRADO5}}/, quadrado);
+    data = data.replace(/{{QUADRADO6}}/, quadrado);
+    data = data.replace(/{{QUADRADO7}}/, quadrado);
+    data = data.replace(/{{QUADRADO8}}/, quadrado);
+    data = data.replace(/{{QUADRADO9}}/, quadrado);
     res.send(data);
   });
 });
