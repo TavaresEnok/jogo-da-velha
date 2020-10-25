cb655468a8250374bfccc0db5d7f3aa5f326bced
Renisson Silva <renissonx@gmail.com>
2020-10-25 17:17:31 -0300

    Guardando no array e mostrando na tela as marcações de X e O

--- server.js
+++ server.js
@@ -3,22 +3,35 @@ const express = require('express');
 
 const app = express();
 
-var elemento = ['x','o','o','o','o','x','x','x','o',];
+var arrayElementos = ['','','','','','','','',''];
+
+cont = 1;
 
 app.get('/', function(req, res) {
+    var divClicada = req.query['div'];
   fs.readFile('index.html', 'utf-8', function(err, data) {
     var quadrado = `
-        <a href="/">
+        <a href="/?div={{INDICE}}">
             <div class="quadrado">{{ELEMENTO}}</div>
         </a>`;
 
     var geral = [   quadrado, quadrado, quadrado,
                     quadrado, quadrado, quadrado,   
                     quadrado, quadrado, quadrado ];
-
+    
     for(i=0; i<9; i++){
-        console.log(geral[i]);
-        geral[i] = geral[i].replace('{{ELEMENTO}}', elemento[i]);
+        if(i == divClicada){
+            if(cont%2!=0){
+                arrayElementos[i] = 'x';
+                cont++;
+            }else{
+                arrayElementos[i] = 'o';
+                cont++;
+            }
+        }
+        geral[i] = geral[i].replace('{{INDICE}}', i);
+        geral[i] = geral[i].replace('{{ELEMENTO}}', arrayElementos[i]);
+        console.log(i == divClicada);
         data = data.replace(`{{QUADRADO${i}}}`, geral[i]);
     }
     res.send(data);
