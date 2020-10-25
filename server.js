311e224e67213c8ab103ee7a89650a0aa49ceb31
Renisson Silva <renissonx@gmail.com>
2020-10-25 18:21:28 -0300

    Criando lógica para definir o vencedor

--- server.js
+++ server.js
@@ -3,39 +3,80 @@ const express = require('express');
 
 const app = express();
 
-var arrayElementos = ['','','','','','','','',''];
+arrayElementos = ['0','1','2','3','4','5','6','7','8'];
+
+finish = false;
 
 cont = 1;
 
 app.get('/', function(req, res) {
     var divClicada = req.query['div'];
-  fs.readFile('index.html', 'utf-8', function(err, data) {
-    var quadrado = `
-        <a href="/?div={{INDICE}}">
-            <div class="quadrado">{{ELEMENTO}}</div>
-        </a>`;
-
-    var geral = [   quadrado, quadrado, quadrado,
-                    quadrado, quadrado, quadrado,   
-                    quadrado, quadrado, quadrado ];
     
-    for(i=0; i<9; i++){
-        if(i == divClicada){
-            if(cont%2!=0){
-                arrayElementos[i] = 'x';
-                cont++;
-            }else{
-                arrayElementos[i] = 'o';
-                cont++;
+    fs.readFile('index.html', 'utf-8', function(err, data) {
+        var quadrado = `
+            <a href="/?div={{INDICE}}">
+                <div class="quadrado">{{ELEMENTO}}</div>
+            </a>`;
+
+        var geral = [   quadrado, quadrado, quadrado,
+                        quadrado, quadrado, quadrado,   
+                        quadrado, quadrado, quadrado ];
+
+        for(i=0; i<9; i++){
+            if(i == divClicada){
+                if(cont%2!=0){
+                    arrayElementos[i] = 'x';
+                    cont++;
+                }else{
+                    arrayElementos[i] = 'o';
+                    cont++;
+                }
             }
+            geral[i] = geral[i].replace('{{INDICE}}', i);
+            if(arrayElementos[i] == 'x' || arrayElementos[i] == 'o'){
+                geral[i] = geral[i].replace('{{ELEMENTO}}', arrayElementos[i]);
+            }else{
+                geral[i] = geral[i].replace('{{ELEMENTO}}', '');
+            }
+            data = data.replace(`{{QUADRADO${i}}}`, geral[i]);
         }
-        geral[i] = geral[i].replace('{{INDICE}}', i);
-        geral[i] = geral[i].replace('{{ELEMENTO}}', arrayElementos[i]);
-        console.log(i == divClicada);
-        data = data.replace(`{{QUADRADO${i}}}`, geral[i]);
-    }
-    res.send(data);
-  });
+        
+        //Condições de vitória
+        if(arrayElementos[1] != '' && arrayElementos[0] == arrayElementos[1] && arrayElementos[1] == arrayElementos[2]){
+            console.log('Venceu 012');
+            finish = true;
+        }else if(arrayElementos[4] != '' && arrayElementos[3] == arrayElementos[4] && arrayElementos[4] == arrayElementos[5]){
+            console.log('Venceu 345');
+            finish = true;
+        }else if(arrayElementos[7] != '' && arrayElementos[6] == arrayElementos[7] && arrayElementos[7] == arrayElementos[8]){
+            console.log('Venceu 678');
+            finish = true;
+        }else if(arrayElementos[3] != '' && arrayElementos[0] == arrayElementos[3] && arrayElementos[3] == arrayElementos[6]){
+            console.log('Venceu 036');
+            finish = true;
+        }else if(arrayElementos[4] != '' && arrayElementos[1] == arrayElementos[4] && arrayElementos[4] == arrayElementos[7]){
+            console.log('Venceu 147');
+            finish = true;
+        }else if(arrayElementos[5] != '' && arrayElementos[2] == arrayElementos[5] && arrayElementos[5] == arrayElementos[8]){
+            console.log('Venceu 258');
+            finish = true;
+        }else if(arrayElementos[4] != '' && arrayElementos[2] == arrayElementos[4] && arrayElementos[4] == arrayElementos[6]){
+            console.log('Venceu 246');
+            finish = true;
+        }else if(arrayElementos[4] != '' && arrayElementos[0] == arrayElementos[4] && arrayElementos[4] == arrayElementos[8]){
+            console.log('Venceu 048');
+            finish = true;
+        }
+
+        if(finish == true){
+            arrayElementos = ['0','1','2','3','4','5','6','7','8'];
+            data = data.replace(`{{RESULTADO}}`, `<a href="/">Reiniciar jogo</a>`);
+            finish = false;
+        }else{
+            data = data.replace(`{{RESULTADO}}`, '');
+        }
+        res.send(data);
+    });
 });
 
 app.listen(3000);
