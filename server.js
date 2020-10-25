7af9ebf5a6e3ccaf352c118b3fa544427ef69833
Renisson Silva <renissonx@gmail.com>
2020-10-25 15:53:23 -0300

    Ajeitando index

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const express = require('express');
 
 const app = express();
 
+var elemento = [
+    ['','',''],
+    ['','',''],
+    ['','','']
+]
+
 app.get('/', function(req, res) {
   fs.readFile('index.html', 'utf-8', function(err, data) {
     var quadrado = `
@@ -10,9 +16,15 @@ app.get('/', function(req, res) {
             <div class="quadrado"></div>
         </a>`;
 
-    var geral = [quadrado, quadrado, quadrado, quadrado, quadrado, quadrado, quadrado, quadrado, quadrado];
+    var geral = [   quadrado, quadrado, quadrado,
+                    quadrado, quadrado, quadrado,   
+                    quadrado, quadrado, quadrado ];
 
     for(i=1; i<=9; i++){
+        elementos = '';
+        // for(j=1; j<=elemento[i].length; j++){
+        //     elementos
+        // }
         data = data.replace(`{{QUADRADO${i}}}`, geral[i-1]);
     }
     res.send(data);
