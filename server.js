b6d00863c2f96fc866606663f2a9a200a706e829
Renisson Silva <renissonx@gmail.com>
2020-10-25 16:37:43 -0300

    Criando array que guarda o X e O

--- server.js
+++ server.js
@@ -3,29 +3,23 @@ const express = require('express');
 
 const app = express();
 
-var elemento = [
-    ['','',''],
-    ['','',''],
-    ['','','']
-]
+var elemento = ['x','o','o','o','o','x','x','x','o',];
 
 app.get('/', function(req, res) {
   fs.readFile('index.html', 'utf-8', function(err, data) {
     var quadrado = `
         <a href="/">
-            <div class="quadrado"></div>
+            <div class="quadrado">{{ELEMENTO}}</div>
         </a>`;
 
     var geral = [   quadrado, quadrado, quadrado,
                     quadrado, quadrado, quadrado,   
                     quadrado, quadrado, quadrado ];
 
-    for(i=1; i<=9; i++){
-        elementos = '';
-        // for(j=1; j<=elemento[i].length; j++){
-        //     elementos
-        // }
-        data = data.replace(`{{QUADRADO${i}}}`, geral[i-1]);
+    for(i=0; i<9; i++){
+        console.log(geral[i]);
+        geral[i] = geral[i].replace('{{ELEMENTO}}', elemento[i]);
+        data = data.replace(`{{QUADRADO${i}}}`, geral[i]);
     }
     res.send(data);
   });
